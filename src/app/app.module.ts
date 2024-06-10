866cb06d3b9f89c21d8fc78b48387a41aa0dc06a
Fran9493 <3979199@alu.murciaeduca.es>
2024-06-10 09:14:06 +0200

    Primer Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { FooterComponent } from './shared/footer/footer.component';
+
+import { LoginComponent } from './auth/login/login.component';
+
+import {ReactiveFormsModule} from '@angular/forms';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { JwtInterceptorService } from './services/auth/jwt-interceptor.service';
+import { ErrorInterceptorService } from './services/auth/error-interceptor.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfesoresGuardiaListComponent } from './profesores-guardia-list/profesores-guardia-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    FooterComponent,
+    DashboardComponent,
+    ProfesoresGuardiaListComponent,
+    
+   
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+     
+  ],
+  providers: [
+    {provide:HTTP_INTERCEPTORS,useClass:JwtInterceptorService,multi:true},
+    {provide:HTTP_INTERCEPTORS,useClass:ErrorInterceptorService,multi:true}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
